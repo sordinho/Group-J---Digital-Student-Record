0b4d6686544618bd7fec8c2862eea656b5cd5b0a
Vittorio <vittoriodl21@gmail.com>
2019-11-06 17:06:30 +0100

    Add Tests directory + foo file for phpunit (version >= 8) general syntax

--- classes/user.class.php
+++ classes/user.class.php
@@ -5,7 +5,7 @@
  *
  */
 require_once(__DIR__."/../site_config.php");
-class user {
+class cuser {
     protected $login_iduser = null;
     protected $username = null;
     protected $password = null;
