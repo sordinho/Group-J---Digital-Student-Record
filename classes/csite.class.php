1f6a037c1c836e21543cbfa1ba828a802f0bb286
Emanuele Munafò <ema.muna95@gmail.com>
2019-11-06 11:16:07 +0100

    Prototype of project structure, first draft for page, site and user management

---
+++ classes/csite.class.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * This is a class file
+ * @file: csite.class.php
+ * @brief: A class file to manage the render of the site
+ * @author: GroupJ 
+ *
+ */
+require_once($_SERVER['DOCUMENT_ROOT']."//site_config.php");
+
+define('APP_RAN', '');//Per verificare se un file � incluso!
+class csite{
+    
+    private $headers;
+    private $footers;
+    private $page;
+    
+    public function __construct(){
+        
+        $this->headers = array();
+        $this->footers = array();
+        
+    }
+    
+    public function __destruct(){
+        
+    //Clean up here!    
+        
+    }
+    
+    public function render(){
+        
+        foreach($this->headers as $header){
+            $h_path = FULL_PATH."/".$header;
+            include $h_path;
+        }
+        /*echo '<div id="main">
+                <div id="nav">';
+            //include("menu.php");
+        echo '</div>';*/
+        $this->page->render();
+        //echo '<div id="extra">';
+        //include("testblabla.php");
+        //echo '</div>';
+
+        foreach($this->footers as $footer){
+            include FULL_PATH."/".$footer;
+        }
+    }
+    
+    // Methods used to add different headers/footer
+    public function addHeader($file){
+        $this->headers[] = $file;
+    }
+    
+    public function addFooter($file){
+        $this->footers[] = $file;
+    }
+    
+    public function setPage($page){
+        $this->page = $page;
+    }
+
+    public function getDefaultSideContent(){
+        include('sidecontent.php');
+    }
+}
+?>
---
+++ classes/user.class.php
@@ -0,0 +1,234 @@
+<?php
+ //error_reporting(E_ALL | E_DEPRECATED | E_STRICT);
+/*
+ *
+ *
+ */
+$hardpath = $_SERVER['DOCUMENT_ROOT'];
+require($hardpath."/site_config.php");
+
+class user {
+    protected $login_iduser = null;
+    protected $username = null;
+    protected $password = null;
+    protected $usergroup = null;
+    protected $base_url = './';
+    
+    public function __construct( $data = array() ) {
+        if( isset( $data['username'] ) ) $this->username = stripslashes( strip_tags( $data['username'] ) );
+        if( isset( $data['password'] ) ) $this->password = stripslashes( strip_tags( $data['password'] ) );
+        //In real not usefull if not creating with Users($_SESSION)
+        if( isset( $data['usergroup'] ) ) $this->usergroup = stripslashes( strip_tags( $data['usergroup'] ) );
+    }
+    private function connectMySQL() {
+        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        /* check connection */
+        if ($mysqli->connect_errno) {
+            printf("Connect failed: %s\n", $mysqli->connect_errno);
+            exit();
+        }
+        return $mysqli;
+    }
+
+    public function storeFormValues($params) {
+        //Memorizzo i parametri
+        $this->__construct($params); 
+    }
+    
+/** Get data for a given user to log in (administrative officer, teacher, parents, admin)
+ *
+ * Service Id is set only for clerk
+ *
+ * @param $post_data
+ * @return userinfo [] 
+ */
+    public function getUserData($id){
+        $userinfo = []; 
+        return $userinfo;
+    }
+/** Verify username and password for a user to consent log in (can be different "types" of users)
+ *
+ *
+ * @param $post_data
+ * @return bool
+ */
+function user_login($post_data) {
+    $username = $post_data["username"];
+    $password = $post_data["password"];
+    $success = false;
+    /*if(!is_email($front_office)){
+        return $success;
+    }*/
+
+    $mysqli = new mysqli(DBAddr, DBUser, DBPassword, DBName);
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        return $success;
+    }
+    // Here using prepared statement to avoid SQLi
+    $query = $mysqli->prepare("SELECT Password FROM Authentication WHERE Username = ?");
+    $query->bind_param('s', $username);
+    $res = $query->execute();
+    if (!$res) {
+        printf("Error message: %s\n", $mysqli->error);
+        return $success;
+    }
+
+    $query->store_result();
+    $query->bind_result($pass);
+    // In case of success there should be just 1 user for a given (username is also a primary key for its table)
+    if ($query->num_rows != 1) {
+        return $success;
+    }
+    $query->fetch();
+    if (password_verify($password, $pass)) {
+        // If here login was successful (hash was verified)
+        $success = true;
+        //set_logged();
+    }
+    $query->close();
+    $mysqli->close();
+
+    // Extends login when needed 
+    //$userinfo = get_user_data($front_office);
+    //set_usergroup($userinfo['usergroup']);
+    set_username($userinfo['username']);             // todo controllare questa riga, era: set_name($userinfo['name']); ----- ma name non esiste
+
+    return $success;
+}
+
+    // verifica sintassi username
+    public function is_username($username){
+        $regex = '/^[a-z0-9\.\-_]{3,30}$/i';
+        return preg_match($regex, $username);
+        }
+    
+    // verifica sintassi email (TRUE se ok)
+    public function is_email($email){
+        $regex = '/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/';
+        return preg_match($regex, $email);
+        }
+        
+    // Retur TRUE if constraints are satisfied
+    public function is_secure_password($password){
+        if(strlen($password)>=5){
+            return TRUE;
+        }
+        else{
+            return FALSE;
+        }
+    }
+            
+    /***********************************
+    VERIFICA DELLO STATO DI LOGIN UTENTE
+    ***********************************/
+    
+    // verifica login
+    public function is_logged(){
+        return isset($_SESSION['id']);
+    }
+    
+    // set login
+    protected function set_logged($id_user){
+        $_SESSION['id'] = $id_user;
+        return;
+    }
+
+
+    //Memorizza nelle sessioni lo username
+    protected function set_username($username){
+        $_SESSION['username'] = $username;
+        return;
+    }
+    
+    protected function set_usergroup($usergroup){
+        $_SESSION['usergroup'] = $usergroup;
+        return;
+    }
+    // Save name for gui?
+    protected function set_name($name){
+        $_SESSION['name'] = ucfirst($name);
+        return;
+    }
+    
+    //Restituisce la mail memorizzata nelle sessioni
+    public function get_username(){
+        return isset($_SESSION['username']) ? $_SESSION['username'] : '';
+    }
+
+    public function get_name(){
+        return isset($_SESSION['name']) ? $_SESSION['name'] : '';
+    }   
+        
+    public function get_usergroup(){
+        return isset($_SESSION['usergroup']) ? $_SESSION['usergroup'] : '';
+    }  
+    public function get_id(){
+        return isset($_SESSION['id']) ? $_SESSION['id'] : '';
+    }
+
+    /* Error handling */
+    // Redirect to error.php (Error handler)
+    public function get_error($id, $noref = null){
+        $html = "<meta http-equiv='refresh' content='0; url=".$this->base_url."/error.php?message=$id' />";
+        if(!empty($noref)){
+            $html = "<meta http-equiv='refresh' content='0; url=".$this->base_url."/error.php?message=$id&noref=1' />";
+        }
+        print $html;
+        //echo "url=".$this->base_url."/error.php?message=$id";
+        exit();
+    }
+
+    /*  Return the logout link */
+    public function get_link_logout(){
+        if($this->is_logged()){
+            return '<a href="'.$this->Urls['logout_page'].'" class="logout">Logout</a>';
+        }
+        return '';
+    }
+    
+
+
+    /*
+    public function sendEmail($to, $subject, $content, $name){
+        //Modifico php.ini per far si da utilizzare il protocollo smtp senza autenticazione.
+        ini_set('SMTP', 'smtp.mydomain.it'); 
+        ini_set('smtp_port', 25); 
+        ini_set('sendmail_from', "info@mydomain.it");
+        //Controllo che l'email sia sintassicamente corretta.
+        if(!$this->is_email($to)) {
+            return false;
+        }
+        $subject = htmlspecialchars($subject);
+        // message
+        $message = '
+        <html>
+        <head>
+            <title>Grazie '.$name.'</title>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        </head>
+        <body>'.
+            $content
+        .'</body><br><br>
+        <small>
+            Questa email è stata generata dal sito <a href=http://mydomain.it>colosi.it</a><br>
+            Per ogni altra informazione o per l\'eliminazione del proprio account andare sul sito o scrivere a<br>
+            info@mydomain.it</small>
+        </html>
+        ';
+        // Modifico il Content-type poichè è una mail in formato html.
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+        // Aggiungo header per non mandarla in SPAM.
+        $headers .= 'To: '.htmlspecialchars($name).' <'.$to.'>' . "\r\n";
+        $headers .= 'From: Digital Student Recorder<info@mydomain.it>' . "\r\n";
+        // Uso la funzione mail per inviare.
+        if(mail($to, $subject, $message, $headers)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }*/
+}
+?>
