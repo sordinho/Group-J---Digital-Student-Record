99caed987670f1dd395e3aae70b332089e75b02c
Emanuele Munafò <ema.muna95@gmail.com>
2019-11-08 17:15:46 +0100

    Added methods to parent class that will be useful for switch child features

--- classes/sparent.php
+++ classes/sparent.php
@@ -3,6 +3,7 @@
 
 class sparent extends user
 {
+    private $parent_id = null;
     private $name = null;
     private $surname = null;
     private $email = null;
@@ -54,5 +55,32 @@ class sparent extends user
         return $content;
     }
 
+    // Register the childs in a session
+    public function retrieve_and_register_childs(){
+
+      $childs = array();
+      $conn = $this->connectMySql();
+      $stmt = $conn->prepare("SELECT StudentID
+              FROM Parent
+              WHERE ID = ?;");
+      $stmt->bind_param('d',$this->parent_id);
+      $stmt->execute();
+      $res = $stmt->get_result();
+      while($row = $res->fetch_row()){
+        array_push($childs, row[0]);
+      }
+      $_SESSION['childsID'] = $childs;
+      //$_SESSION['childNames'] = $child_names;
+    }
+    // Register a child as the current to view and analyze by saving the studentID into the session
+    public function set_current_child($childID){
+      $_SESSION['curChild'] = $childID;
+      //$_SESSION['childNames'] = $child_names;
+
+    }
+    // Return -1 if no current child was choosen until now, return the studentID of the child otherwise
+    public function get_current_child($childID){
+        return isset($_SESSION['curChild']) ? $_SESSION['curChild'] : -1; 
+    }
 
 }
