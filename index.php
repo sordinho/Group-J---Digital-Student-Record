89c708c07b8559a0d1f030bcea79a097304ba383
Davide Sordi <s228457@studenti.polito.it>
2019-11-08 23:50:20 +0100

    Update user class with new get and set.

--- index.php
+++ index.php
@@ -6,46 +6,48 @@ initialize_site($site);
 $page = new cpage("Home");
 $site->setPage($page);
 
-$content ='<a href="usergroup/parent/parent.php">parentPage</a>';
+$content = '<a href="usergroup/parent/parent.php">parentPage</a>';
 
 $page->setContent($content);
 $site->render();
 
-if(!isset($_SESSION['id']) && isset($_POST['username'])) {
-    echo "You just tried login";
-    $usr = new user;
-    $usr->storeFormValues( $_POST );
+if (!isset($_SESSION['id']) && isset($_POST['username'])) {
+	echo "You just tried login";
+	$usr = new user;
+	$usr->storeFormValues($_POST);
 
-    $post_data["username"] = $_POST['username'];
-    $post_data["password"] = $_POST['password'];
+	$post_data["username"] = $_POST['username'];
+	$post_data["password"] = $_POST['password'];
 
-    echo $_POST['username'] . $_POST['password'];
-    
-    if($usr->user_login($post_data)) {
-        $usergroup = $usr->get_usergroup();
-        $url = "/index.php";
-        switch($usergroup){
-            case "Teacher":
-                $url = "/usergroup/teacher/index.php";
-                break;
-            case "Parent":
-                //$url = "/usergroup/parent/parent.php";
-                break;
-            case "TODO":
-                //$url = "/TODO.php";
-                break; 
-        }
-        echo "Success";
-        $html = "<meta http-equiv='refresh' content='1; url=".PLATFORM_PATH.$url."' />";
-        die($html);
-    } else {
-        $usr->get_error(11);
-    }
+	echo $_POST['username'] . $_POST['password'];
+
+	if ($usr->user_login($post_data)) {
+		$usergroup = $usr->get_usergroup();
+		$url = "/index.php";
+		switch ($usergroup) {
+			case "Teacher":
+				$url = "/usergroup/teacher/index.php";
+				break;
+			case "Parent":
+				//$url = "/usergroup/parent/parent.php";
+				break;
+			case "TODO":
+				//$url = "/TODO.php";
+				break;
+		}
+		echo "Success";
+		$html = "<meta http-equiv='refresh' content='1; url=" . PLATFORM_PATH . $url . "' />";
+		die($html);
+	} else {
+		$usr->get_error(11);
+	}
 } else {
-    $usr = new user();
-    echo "Authenticated?".$usr->is_logged();
-    echo "\nUsergroup: ".$usr->get_usergroup();
-    echo "\nUsername: ".$usr->get_username();
+	$usr = new user();
+	echo "Authenticated?" . $usr->is_logged();
+	echo "<br>Usergroup: " . $usr->get_usergroup();
+	echo "<br>Username: " . $usr->get_username();
+	echo "<br>ID: " . $usr->get_id();
+	echo "<br>Base_URL: " . $usr->get_base_url();
 
 }
 
