b88cd417e7009258fa59823d5ec7568562acb584
francesco <frenky.riba.95@gmail.com>
2019-11-09 10:58:15 +0100

    tweaks

--- usergroup/parent/index.php
+++ usergroup/parent/index.php
@@ -31,7 +31,7 @@ while($row = $grades->fetch_row()){
       <th scope="row">'.$row[1].'</th>
       <td>'.$row[0].'</td>
       <td>TODO</td>
-      <td>'.$row[3].'</td>
+      <td>'.$row[2].'</td>
       <td>TODO</td>
     </tr>';
 }
