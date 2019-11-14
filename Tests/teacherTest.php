5e4f91d9b7870dab1faa565fcd90ef33cd2ab243
Francesco <frenky.riba.95@gmail.com>
2019-11-14 17:40:11 +0100

    tweaks teacherTest

--- Tests/teacherTest.php
+++ Tests/teacherTest.php
@@ -8,44 +8,43 @@ require_once '../classes/teacher.class.php';
 
 class teacherTest extends TestCase {
 
-	public static function setUpBeforeClass(): void {
-		createTestDatabase();
-	}
+    public static function setUpBeforeClass(): void {
+        createTestDatabase();
+    }
 
-	public static function tearDownAfterClass(): void {
-		dropTestDatabase();
-	}
+    public static function tearDownAfterClass(): void {
+        dropTestDatabase();
+    }
 
 
-	public function testModify_lecture_topic() {
-		$_SESSION["teacherID"] = 1;
-		$topicID = 1;
-		$_SESSION["id"]=3;
-		$description = "Test topic description";
-		$modifiedDescription = "MODIFIED Test topic description";
-		$teacherObject = new Teacher($_SESSION);
-		$teacherID = $teacherObject->get_teacher_ID();
-		$timestamp = date("Y-m-d H:i:s");
+    public function testModify_lecture_topic() {
+        $_SESSION["teacherID"] = 1;
+        $topicID = 1;
+        $_SESSION["id"]=3;
+        $description = "Test topic description";
+        $modifiedDescription = "MODIFIED Test topic description";
+        $teacherObject = new Teacher($_SESSION);
+        $teacherID = $teacherObject->get_teacher_ID();
+        $timestamp = date("Y-m-d H:i:s");
 
-		// Insert a topic record to modify
-		$this->assertNotNull($teacherObject->insert_new_lecture_topic($description, $topicID, $timestamp));
+        // Insert a topic record to modify
+        $this->assertNotNull($teacherObject->insert_new_lecture_topic($description, $topicID, $timestamp));
 
-		// Try to modify the topic record
-		$teacherObject->modify_lecture_topic($modifiedDescription,);
+        // Try to modify the topic record
+        $teacherObject->modify_lecture_topic($modifiedDescription,);
 
-	}
+    }
 
-	public function testInsert_new_lecture_topic()
-	{
-		//TODO
-		// variables
+    public function testInsert_new_lecture_topic()
+    {
+        //TODO
+        // variables
 //        $login["unsername"]="TeacherEmail1";
 //        $login["password"]="frontoffice1";
 //
 //        $login=array("TeacherEmail1","frontoffice1");
 //        $user=new user($login);
 //        $user->user_login($login);
-<<<<<<< HEAD
         $_SESSION["teacherID"]=1;
         $teacherObject = new Teacher($_SESSION);
         $topicID=1;
@@ -55,25 +54,13 @@ class teacherTest extends TestCase {
         $specificClassID=1;
         $teacherID=$teacherObject->get_teacher_ID();
         $topicRecordID=-1;
-=======
-		$_SESSION["teacherID"]=1;
-		$teacherObject = new Teacher($_SESSION);
-		$topicID=1;
-		$description="Lecture 1 topic 1";
-		$dateActualDate=date("Y-m-d H:i:s");
-		$dateActualString=strtotime($dateActualDate);
-		$specificClassID=1;
-		$teacherID=$teacherObject->get_teacher_ID();
 
->>>>>>> master
+        //TODO
+        //printf("%d",$teacherObject->get_teacher_ID());
 
-		//TODO
-		//printf("%d",$teacherObject->get_teacher_ID());
+        //perform insertion in the DB
+        $this->assertNotNull($teacherObject->insert_new_lecture_topic($description,$topicID,$dateActualDate));
 
-		//perform insertion in the DB
-		$this->assertNotNull($teacherObject->insert_new_lecture_topic($description,$topicID,$dateActualDate));
-
-<<<<<<< HEAD
 
 
 
@@ -90,9 +77,10 @@ class teacherTest extends TestCase {
             $topicRecordID=$row[0];
         }
         $res->close();
+        $conn->close();
 
 
-
+        $conn=connectMySQLTests();
         $stmt = $conn->prepare("SELECT COUNT(*) FROM TopicRecord WHERE ID = ?;");
         $stmt->bind_param('i', $topicRecordID);
         $stmt->execute();
@@ -101,7 +89,7 @@ class teacherTest extends TestCase {
             return false;
         }else{
             $row=$res->fetch_row();
-            //printf("Count: %d\n",$row[0]);
+            printf("Count: %d\n",$row[0]);
             $this->assertEquals($row[0],1,"Test superato con successo!");
         }
         $res->close();
@@ -109,33 +97,6 @@ class teacherTest extends TestCase {
 
         //Prints for debug
         printf("TeacherID: %d\nDateActual: %s\nDescription: %s\nTopicID: %d\nSpecifiClassID: %d",$teacherID,$dateActualDate,$description,$topicID,$specificClassID);
-=======
-		//Prints for debug
-		printf("TeacherID: %d\nDateActual: %s\nDescription: %s\nTopicID: %d\nSpecifiClassID: %d",$teacherID,$dateActualDate,$description,$topicID,$specificClassID);
->>>>>>> master
 
-
-//        $conn = $teacherObject->connectMySQL();
-//
-//        if ($result = $conn->query($sql)) {
-//            $row = $result->fetch_array();
-//            $value = $row[0];
-//
-//            $result->close();
-//            return  $value;
-//        } else {
-//            printf("Error message: %s\n", $conn->error);
-//        }
-
-		//Verify that the insertion has been executed correctly
-		$count = perform_SELECT_return_single_value(
-			"SELECT COUNT(*) FROM topicrecord WHERE TeacherID=$teacherID AND Timestamp=$dateActualString AND Description='$description' AND TopicID=$topicID AND SpecificClassID=$specificClassID"
-		);
-
-		printf("\n%d",$count);
-
-
-		$this->assertEquals("a", "a");
-
-	}
+    }
 }
