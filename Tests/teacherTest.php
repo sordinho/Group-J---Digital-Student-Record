0c479aafb1bfdf3d23c46772daeaea3c0e1d4c95
Francesco <frenky.riba.95@gmail.com>
2019-11-14 14:39:48 +0100

    tweaks teacherTest

--- Tests/teacherTest.php
+++ Tests/teacherTest.php
@@ -31,23 +31,28 @@ class teacherTest extends TestCase
         //TODO
         $_SESSION["teacherID"]=1;
         $teacherObject = new Teacher($_SESSION);
+        $topicID=1;
+        $dateActual="2019-11-14 14:10:10";
+        $specificClassID=1;
+        $description="Lecture 1 topic 1";
+        $teacherID=$teacherObject->get_teacher_ID();
         //TODO
-        printf("%d",$teacherObject->get_teacher_ID());
-        //$
-        //printf("Ciao");
-        // perform insertion of one value
-        //$teacherObject->
+        //printf("%d",$teacherObject->get_teacher_ID());
 
-//        /*$max_before = perform_SELECT_return_single_value(
-//            "SELECT MAX(TicketNumber) FROM Queue WHERE ServiceID =1;"
-//        );
-//
+        //perform insertion in the DB
+        perform_INSERT_or_DELETE("INSERT INTO topicrecord(TeacherID, Timestamp, Description, TopicID, SpecifiClassID) VALUES($teacherID,$dateActual,$description,$topicID,$specificClassID");
+
+        //Verify that the insertion has been executed correctly
+        $count = perform_SELECT_return_single_value(
+            "SELECT COUNT(*) FROM topicrecord WHERE TeacherID=$teacherID AND Timestamp='$dateActual' AND Description='$description' AND TopicID=$topicID AND SpecifiClassID=$specificClassID"
+        );
+
 //        //add_top("Packages");
 //        $max_after = perform_SELECT_return_single_value(
 //            "SELECT MAX(TicketNumber) FROM Queue WHERE ServiceID =1;"
 //        );
 //         printf("\n\n%d---->%d\n\n",$max_before,$max_after);
-//        $this->assertTrue($max_after == ($max_before + 1), "TestQueue: test_add_top not performed correctly or not performed");*/
+        //$this->assertTrue($max_after == ($max_before + 1), "TestQueue: test_add_top not performed correctly or not performed");*/
 
 
 
