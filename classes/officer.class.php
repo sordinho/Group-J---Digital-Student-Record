360320c39e51d7387af9f9a915bcca76df4ba207
Vittorio <vittoriodl21@gmail.com>
2019-11-14 17:28:08 +0100

    Update uploadParentCredentials, update officier class

--- classes/officer.class.php
+++ classes/officer.class.php
@@ -13,4 +13,48 @@ class Officer extends user {
 	public function get_officer_ID() {
 		return isset($_SESSION['officerID']) ? $_SESSION['officerID'] : -1;
 	}
+/*
+ * questa funzione riceve nome, cognome, email, informazioni sui figli e numero di figli
+ * e provvede a inserire un nuovo utente nella tabella user e un nuovo parent nella tabella parent (una entry per ogni
+ * figlio)
+ * */
+	public function register_new_parent($name,$surname,$email,$child_info,$child_N){
+        //todo check correctness
+	    if(!isset($name)||!isset($surname)||!isset($email)||!isset($child_info)||empty($child_info))
+	        return false;
+	    $conn = $this->connectMySQL();
+        $stmt = $conn->prepare("INSERT INTO User (Name, Surname, Email, Password, UserGroup) VALUES (?,?,?,?);");
+        $stmt->bind_param('sssss', $name, $surname, $email, '', 'parent');
+        $stmt->execute();
+        if(!$stmt->get_result())
+            return false;
+        $stmt = $conn->prepare("SELECT ID
+                                      FROM User
+                                      WHERE Name = ?
+                                        AND Surname= ? 
+                                        AND Email = ?
+                                        AND UserGroup = 'parent';");
+        $stmt->bind_param('sss', $name, $surname, $email);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        if($res->num_rows<=0)
+            return false;
+        $parent_id = $res->fetch_row()[0];
+        for($i = 0; $i < $child_N;$i++){
+            $stmt2 =$conn->prepare("SELECT ID FROM Student WHERE CF = ?;");
+            $stmt2->bind_param('s',$child_info['cf_'.$i]);
+            $stmt2->execute();
+            $res = $stmt2->get_result();
+            if($res->num_rows <=0)
+                return false;
+            $student_id = $res->fetch_row()[0];
+            $stmt2 = $conn->prepare("INSERT INTO Parent(StudentID, UserID) VALUES(?,?);");
+            $stmt2->bind_param('ii',$student_id,$parent_id);
+            $stmt2->execute();
+            if(!$stmt2->get_result())
+                return false;
+        }
+
+        return true;//True || False
+    }
 }
--- usergroup/administrativeOfficier/uploadParentCredentials.php
+++ usergroup/administrativeOfficier/uploadParentCredentials.php
@@ -5,33 +5,78 @@ $site = new csite();
 initialize_site($site);
 $page = new cpage("Register new parent");
 $site->setPage($page);
+/*$officier = new Officer();
+if(!$officier->is_logged()){
+    $content = '
+    <div class="alert alert-warning" role="warning">
+        You are not authorized. If you are in a hurry <a href="./index.php" class="alert-link">just click here!</a>
+    </div> ';
+    $content .= "<meta http-equiv='refresh' content='2; url=" . PLATFORM_PATH . "' />";
+    $page->setContent($content);
+    $site->render();
+    render_page($content, '');
+}*/
 $num = 1;
+$content="";
+
 if(!empty($_POST)){
     $content = "OK";
-    $parent_N = $_POST['materialRegisterFormFirstName'];
-    $parent_S = $_POST['materialRegisterFormLastName'];
-    $parent_email = $_POST['materialRegisterFormEmail'];
-    $child_N = $_POST['materialRegisterFormChildNumber'];
+    $parent_N = $_POST['parent_first_name'];
+    $parent_S = $_POST['parent_last_name'];
+    $parent_email = $_POST['parent_email'];
+    $child_N = $_POST['children'];
     $content.="<p>Name: ".$parent_N."</p>";
     $content.="<p>Surname: ".$parent_S."</p>";
     $content.="<p>Email: ".$parent_email."</p>";
     $content.="<p>Child#: ".$child_N."</p>";
     $child_info = array();
     for($i = 0; $i<$child_N;$i++){
-        $child_info['FirstName'.$i]= $_POST['materialRegisterFormFirstNameChild'.$i];
-        $child_info['LastName'.$i]= $_POST['materialRegisterFormLastNameChild'.$i];
-        $child_info['CF'.$i] = $_POST['materialRegisterFormCF'.$i];
-        $content.="<p>Name: ".$child_info['FirstName'.$i].'</p>';
-        $content.="<p>Surname: ".$child_info['LastName'.$i].'</p>';
-        $content.="<p>CF: ".$child_info['CF'.$i].'</p>';
+        $child_info['first_name_'.$i]= $_POST['first_name_child_'.$i];
+        $child_info['last_name_'.$i]= $_POST['last_name_child_'.$i];
+        $child_info['cf_'.$i] = $_POST['cf_'.$i];
+        $content.="<p>Name: ".$child_info['first_name_'.$i].'</p>';
+        $content.="<p>Surname: ".$child_info['last_name_'.$i].'</p>';
+        $content.="<p>CF: ".$child_info['cf_'.$i].'</p>';
 
     }
-    //$content="";
-
+    //TODO creare uno user con dette credenziali e aggiungere le varie entry nella tabella parent
+    $res = true; //todo edit
+    // $res = officier->register_new_parent(...);
+    if($res){
+        header("Location: uploadParentCredentials.php?operation_result=1");
+        die();
+    }
 }else{
-    if (isset($_GET['childN']))
-        $num = $_GET['childN'];
-    $content = <<<OUT
+    if(isset($_GET['operation_result'])){
+        switch ($_GET['operation_result']){
+            case 1:
+                $content.=<<<OUT
+<div class="alert alert-success" role="alert">
+  Parent successfully uploaded. <a href="uploadParentCredentials.php" class="alert-link">Add another parent</a> or <a href="index.php" class="alert-link">back to your homepage.</a>
+</div>
+OUT;
+                break;
+            case 0:
+                $content.=<<<OUT
+<div class="alert alert-danger" role="alert">
+ Error in uploading parent's master data. <a href="#" class="alert-link">Retry </a> or <a href="index.php" class="alert-link">back to your homepage.</a>
+</div>
+OUT;
+
+                break;
+            default:
+                $content.=<<<OUT
+<div class="alert alert-dark" role="alert">
+  Operation not allowed.
+</div>
+OUT;
+        }
+
+
+    } else {
+        if (isset($_GET['childN']))
+            $num = $_GET['childN'];
+        $content .= <<<OUT
 <!-- Material form register -->
 <div class="card">
     <script type="text/javascript"><!--
@@ -54,14 +99,14 @@ if(!empty($_POST)){
                 <div class="col">
                     <!-- First name -->
                     <div class="md-form">
-                        <input type="text" id="materialRegisterFormFirstName" class="form-control">
+                        <input type="text" id="materialRegisterFormFirstName" name="parent_first_name" class="form-control">
                         <label for="materialRegisterFormFirstName">First name</label>
                     </div>
                 </div>
                 <div class="col">
                     <!-- Last name -->
                     <div class="md-form">
-                        <input type="text" id="materialRegisterFormLastName" class="form-control">
+                        <input type="text" id="materialRegisterFormLastName" name="parent_last_name" class="form-control">
                         <label for="materialRegisterFormLastName">Last name</label>
                     </div>
                 </div>
@@ -69,19 +114,19 @@ if(!empty($_POST)){
 
             <!-- E-mail -->
             <div class="md-form mt-0">
-                <input type="email" id="materialRegisterFormEmail" class="form-control">
+                <input type="email" id="materialRegisterFormEmail" name="parent_email" class="form-control">
                 <label for="materialRegisterFormEmail">E-mail</label>
             </div>
             
             <!-- Children number-->
             <div class="md-form mt-0">
-                <input type="number" id="materialRegisterFormChildNumber" class="form-control" onchange="displayChildrenForm(this)" value="$num">
+                <input type="number" id="materialRegisterFormChildNumber" name="children" class="form-control" onchange="displayChildrenForm(this)" value="$num">
                 <label for="materialRegisterFormEmail">Number of children</label>
             </div>
 OUT;
 
-    for ($i = 0; $i < $num; $i++) {
-        $content .= <<<OUT
+        for ($i = 0; $i < $num; $i++) {
+            $content .= <<<OUT
             <hr>
             
             <p class="card-body info-color white-text text-center py-4">Child $i</p>
@@ -89,14 +134,14 @@ OUT;
                 <div class="col">
                     <!-- First name -->
                     <div class="md-form">
-                        <input type="text" id="materialRegisterFormFirstNameChild$i" class="form-control">
+                        <input type="text" id="materialRegisterFormFirstNameChild$i" name="first_name_child_$i" class="form-control">
                         <label for="materialRegisterFormFirstName">Child first name</label>
                     </div>
                 </div>
                 <div class="col">
                     <!-- Last name -->
                     <div class="md-form">
-                        <input type="text" id="materialRegisterFormLastNameChild$i" class="form-control">
+                        <input type="text" id="materialRegisterFormLastNameChild$i" name="last_name_child_$i" class="form-control">
                         <label for="materialRegisterFormLastName">Child last name</label>
                     </div>
                 </div>
@@ -104,12 +149,12 @@ OUT;
             
             <!-- CF -->
             <div class="md-form mt-0">
-                <input type="text" id="materialRegisterFormCF$i" class="form-control">
+                <input type="text" id="materialRegisterFormCF$i" name="cf_$i" class="form-control">
                 <label for="materialRegisterFormEmail">Child fiscal code</label>
             </div>
 OUT;
-    }
-    $content .= <<<OUT
+        }
+        $content .= <<<OUT
     <!-- Sign up button -->
             <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Submit</button>
 
@@ -121,19 +166,7 @@ OUT;
 </div>
 <!-- Material form register -->
 OUT;
-    /*here content*/
-
-//1 get generated credentials
-// $officier = new AdministrativeOfficier();
-// $res = $officier->getGeneratedCredentials();
-//2 show generated credentials in a tabular fashion
-// while($row = $res->fetch_row()){
-//      print a table
-// }
-//3 confirm the operation
-// ask for user confirmation
-//3.a send mail
-// IF user confirms --> notify mailer class
+    }
 }
 $page->setContent($content);
 $site->render();
