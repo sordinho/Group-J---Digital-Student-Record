21fe0242c3dc7422a940cca728e0a4f9e35caec2
Francesco <frenky.riba.95@gmail.com>
2019-11-14 17:13:31 +0100

    tweaks teacherTest, added connectMySQLTests in testUtility.php

--- Tests/teacherTest.php
+++ Tests/teacherTest.php
@@ -53,7 +53,7 @@ class teacherTest extends TestCase {
         $dateActualString=strtotime($dateActualDate);
         $specificClassID=1;
         $teacherID=$teacherObject->get_teacher_ID();
-
+        $topicRecordID=-1;
 
         //TODO
         //printf("%d",$teacherObject->get_teacher_ID());
@@ -61,31 +61,41 @@ class teacherTest extends TestCase {
         //perform insertion in the DB
         $this->assertNotNull($teacherObject->insert_new_lecture_topic($description,$topicID,$dateActualDate));
 
+
+
+
+        $conn=connectMySQLTests();
+
+        $stmt = $conn->prepare("SELECT ID FROM TopicRecord WHERE Timestamp = ?;");
+        $stmt->bind_param('s', $dateActualDate);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        if ($res->num_rows <= 0) {
+            return false;
+        }else{
+            $row=$res->fetch_row();
+            $topicRecordID=$row[0];
+        }
+        $res->close();
+
+
+
+        $stmt = $conn->prepare("SELECT COUNT(*) FROM TopicRecord WHERE ID = ?;");
+        $stmt->bind_param('i', $topicRecordID);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        if ($res->num_rows <= 0) {
+            return false;
+        }else{
+            $row=$res->fetch_row();
+            //printf("Count: %d\n",$row[0]);
+            $this->assertEquals($row[0],1,"Test superato con successo!");
+        }
+        $res->close();
+        $conn->close();
+
         //Prints for debug
-        printf("TeacherID: %d\nDateActual: %s\nDescription: %s\nTopicID: %d\nSpecifiClassID: %d",$teacherID,$dateActualDate,$description,$topicID,$specificClassID);
-
-
-//        $conn = $teacherObject->connectMySQL();
-//
-//        if ($result = $conn->query($sql)) {
-//            $row = $result->fetch_array();
-//            $value = $row[0];
-//
-//            $result->close();
-//            return  $value;
-//        } else {
-//            printf("Error message: %s\n", $conn->error);
-//        }
-
-        //Verify that the insertion has been executed correctly
-        $count = perform_SELECT_return_single_value(
-            "SELECT COUNT(*) FROM topicrecord WHERE TeacherID=$teacherID AND Timestamp=$dateActualString AND Description='$description' AND TopicID=$topicID AND SpecificClassID=$specificClassID"
-        );
-
-        printf("\n%d",$count);
-
-
-		$this->assertEquals("a", "a");
+        //printf("TeacherID: %d\nDateActual: %s\nDescription: %s\nTopicID: %d\nSpecifiClassID: %d",$teacherID,$dateActualDate,$description,$topicID,$specificClassID);
 
 	}
 }
