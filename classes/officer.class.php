0b53c93f69a6a69cb21f6fd09bbe04db2c5a44f7
Emanuele Munafò <ema.muna95@gmail.com>
2019-11-14 17:49:03 +0100

    added enroll student

--- classes/officer.class.php
+++ classes/officer.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class Officer extends user {
+class officer extends user {
 
 	private $officer_id = null;
 
@@ -13,4 +13,20 @@ class Officer extends user {
 	public function get_officer_ID() {
 		return isset($_SESSION['officerID']) ? $_SESSION['officerID'] : -1;
 	}
+
+	// Enroll a new student (anagraphic date saved into db)
+	public function enroll_student($student_info) {
+		if(!array_key_exists("name", $student_info) ){
+			return false;
+		}
+		$classID = -1;
+		// immetricolation info inserted also now? 
+		$actual_year= strtotime(date("Y"));
+		$conn = $this->connectMySQL();
+		//(`ID`, `Name`, `Surname`, `AverageLastSchool`, `CF`, `SpecificClassID`) 
+		$stmt = $conn->prepare("INSERT INTO Student(Name, Surname, AverageLastSchool, CF, SpecificClassID) VALUES (?,?,?,?,?)");
+		$stmt->bind_param('ssfsi', $student_info["name"], $student_info["surname"], $student_info["avgLastSchool"], $student_info["CF"], $classID);
+		$stmt->execute();
+		return $stmt->get_result();//True || False
+	}	
 }
