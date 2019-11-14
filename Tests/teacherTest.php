a61d3eb4805a9a21b856f12082a496818cb517d1
Davide Sordi <s267570@studenti.polito.it>
2019-11-14 17:30:56 +0100

    Restored teacher test

--- Tests/teacherTest.php
+++ Tests/teacherTest.php
@@ -3,6 +3,8 @@
 
 use PHPUnit\Framework\TestCase;
 require_once 'testUtility.php';
+require_once '../classes/user.class.php';
+require_once '../classes/teacher.class.php';
 
 class teacherTest extends TestCase {
 
@@ -31,36 +33,36 @@ class teacherTest extends TestCase {
 		// Try to modify the topic record
 		$teacherObject->modify_lecture_topic($modifiedDescription,);
 
-    }
+	}
 
-    public function testInsert_new_lecture_topic()
-    {
-        //TODO
-        // variables
+	public function testInsert_new_lecture_topic()
+	{
+		//TODO
+		// variables
 //        $login["unsername"]="TeacherEmail1";
 //        $login["password"]="frontoffice1";
 //
 //        $login=array("TeacherEmail1","frontoffice1");
 //        $user=new user($login);
 //        $user->user_login($login);
-        $_SESSION["teacherID"]=1;
-        $teacherObject = new Teacher($_SESSION);
-        $topicID=1;
-        $description="Lecture 1 topic 1";
-        $dateActualDate=date("Y-m-d H:i:s");
-        $dateActualString=strtotime($dateActualDate);
-        $specificClassID=1;
-        $teacherID=$teacherObject->get_teacher_ID();
+		$_SESSION["teacherID"]=1;
+		$teacherObject = new Teacher($_SESSION);
+		$topicID=1;
+		$description="Lecture 1 topic 1";
+		$dateActualDate=date("Y-m-d H:i:s");
+		$dateActualString=strtotime($dateActualDate);
+		$specificClassID=1;
+		$teacherID=$teacherObject->get_teacher_ID();
 
 
-        //TODO
-        //printf("%d",$teacherObject->get_teacher_ID());
+		//TODO
+		//printf("%d",$teacherObject->get_teacher_ID());
 
-        //perform insertion in the DB
-        $this->assertNotNull($teacherObject->insert_new_lecture_topic($description,$topicID,$dateActualDate));
+		//perform insertion in the DB
+		$this->assertNotNull($teacherObject->insert_new_lecture_topic($description,$topicID,$dateActualDate));
 
-        //Prints for debug
-        printf("TeacherID: %d\nDateActual: %s\nDescription: %s\nTopicID: %d\nSpecifiClassID: %d",$teacherID,$dateActualDate,$description,$topicID,$specificClassID);
+		//Prints for debug
+		printf("TeacherID: %d\nDateActual: %s\nDescription: %s\nTopicID: %d\nSpecifiClassID: %d",$teacherID,$dateActualDate,$description,$topicID,$specificClassID);
 
 
 //        $conn = $teacherObject->connectMySQL();
@@ -75,14 +77,15 @@ class teacherTest extends TestCase {
 //            printf("Error message: %s\n", $conn->error);
 //        }
 
-        //Verify that the insertion has been executed correctly
-        $count = perform_SELECT_return_single_value(
-            "SELECT COUNT(*) FROM topicrecord WHERE TeacherID=$teacherID AND Timestamp=$dateActualString AND Description='$description' AND TopicID=$topicID AND SpecificClassID=$specificClassID"
-        );
+		//Verify that the insertion has been executed correctly
+		$count = perform_SELECT_return_single_value(
+			"SELECT COUNT(*) FROM topicrecord WHERE TeacherID=$teacherID AND Timestamp=$dateActualString AND Description='$description' AND TopicID=$topicID AND SpecificClassID=$specificClassID"
+		);
 
-        printf("\n%d",$count);
+		printf("\n%d",$count);
 
 
 		$this->assertEquals("a", "a");
-    }
+
+	}
 }
