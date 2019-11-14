7f5fbe55df6363a1cd23c88649695cd2e4d3d9ec
Francesco <frenky.riba.95@gmail.com>
2019-11-14 11:03:16 +0100

    created teacherTest

---
+++ Tests/teacherTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+require_once 'testUtility.php';
+
+class teacherTest extends TestCase
+{
+
+    public function test__construct()
+    {
+        //TODO
+        $this->assertEquals("a","a");
+    }
+
+    public function testModify_lecture_topic()
+    {
+        //TODO
+        $this->assertEquals("a","a");
+
+    }
+
+    public function testInsert_new_lecture_topic()
+    {
+        //TODO
+        $this->assertEquals("a","a");
+
+    }
+}
