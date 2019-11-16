2556e4bdfad51c40bf205396de14ed0c08151439
Francesco <frenky.riba.95@gmail.com>
2019-11-16 18:30:54 +0100

    Added classCompositionModification page and link, created get_Class_List() function in officer.class.php

--- usergroup/officer/index.php
+++ usergroup/officer/index.php
@@ -7,7 +7,8 @@ $page = new cpage("Administrative Officer homepage");
 $site->setPage($page);
 
 $content = "<div class='container article-clean'>Administrative Officer</div>
-            <a href=\"studentEnrollment.php\" class=\"badge badge-secondary\">Student Enrollment Page</a>";
+            <a href=\"studentEnrollment.php\" class=\"badge badge-secondary\">Student Enrollment Page</a>
+            <a href=\"classCompositionModification.php\" class=\"badge badge-secondary\">Class Composition Modification Page</a>";
 
 $page->setContent($content);
 $site->render();
