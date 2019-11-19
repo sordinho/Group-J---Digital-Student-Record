d483cbd7ca7e1d1dbe2a1ff44f96a9b9d4074b9e
Emanuele Munafò <ema.muna95@gmail.com>
2019-11-19 15:48:20 +0100

    dummy text removed from home

--- usergroup/teacher/index.php
+++ usergroup/teacher/index.php
@@ -27,15 +27,7 @@ $content = '<div class="container article-clean">
 			<h1 class="text-center">What can i do?</h1>
 			<p class="text-center"><span class="by"></span> <a href="#"></a><span class="date"></span></p><!--<img class="img-fluid" src="assets/img/desk.jpg">--></div>
 		<div class="text">
-			<p>On the sidebar you can quickly perform many different actions. <span style="text-decoration: underline;">Vivamus</span> ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.
-				Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-			<p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac lacus. <strong>Ut vehicula rhoncus</strong> elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit <em>pulvinar dict</em> vel in justo. Vestibulum ante
-				ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-			<h2>Aliquam In Arcu </h2>
-			<p>Suspendisse vel placerat ligula. Vivamus ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-				posuere cubilia Curae.</p>
-			<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Suspendisse vel placerat ligula. Vivamus ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar
-				dictum vel in justo.</p>
+			<p>On the sidebar you can quickly perform many different actions.</p>
 		</div>
 	</div>
 </div>
