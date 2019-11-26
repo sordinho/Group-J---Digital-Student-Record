b9fdd669c542a099c887d308db4cbf1c5c38ef73
Emanuele Munafò <ema.muna95@gmail.com>
2019-11-26 11:00:19 +0100

    Added failure condition

--- usergroup/parent/homeworkAPI.php
+++ usergroup/parent/homeworkAPI.php
@@ -4,12 +4,8 @@ header('Content-Type: application/json');
 require_once("../../config.php");
 
 $sparent = new sparent();
-if(!$result){
-}else{
-    echo json_encode(array('status' => 'success','message'=> 'The group has been removed'));
-}
 
-if (!$sparent->is_logged() || $sparent->get_parent_ID() == -1) {
+if (!$sparent->is_logged() || $sparent->get_parent_ID() == -1 || $sparent->get_current_child() == -1) {
 
     echo json_encode(array('status' => 'error','message'=> 'Not authorized'));
     exit();
