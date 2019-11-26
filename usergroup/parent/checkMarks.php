a1944a7f7f219bd1997850d6b0bf2091f86d7c8e
Emanuele Munafò <ema.muna95@gmail.com>
2019-11-26 10:49:56 +0100

    Refractor

--- usergroup/parent/checkMarks.php
+++ usergroup/parent/checkMarks.php
@@ -9,7 +9,6 @@ $page = new cpage("Marks");
 $site->setPage($page);
 $sparent = new sparent();
 
-
 if (!$sparent->is_logged() || $sparent->get_parent_ID() == -1) {
 	$content = '
     <div class="alert alert-warning" role="warning">
@@ -18,44 +17,44 @@ if (!$sparent->is_logged() || $sparent->get_parent_ID() == -1) {
 	$content .= "<meta http-equiv='refresh' content='2; url=" . PLATFORM_PATH . "' />";
 	$page->setContent($content);
 	$site->render();
-	exit();
-} else {
-	$content = '
-  <table class="table">
-    <thead class="thead-dark">
-      <tr>
-        <th scope="col">Date</th>
-        <th scope="col">Grade</th>
-        <th scope="col">Subject</th>
-        <th scope="col">Professor</th>
-      </tr>
-    </thead>
-    <tbody>';
-	$cur_child = $sparent->get_current_child();
-
-	//Note: a warning message is already displayed in the index, so for now an ugly die is enough
-	if ($cur_child == -1) {
-		die('<div class="alert alert-danger" role="alert">Please, select a student</div>');
-	}
-
-	$grades = $sparent->get_grades($cur_child);
-	if ($grades) {
-		foreach ($grades as $i => $row) {
-			//TODO: CHANGE WHEN get_grades return correct data
-			//NOT t.Name, Mark, Timestamp, u.Surname (u.surname is useless)
-			$content .= '<tr>
-          <th scope="row">' . $row['Timestamp'] . '</th>
-          <td>' . $row['Mark'] . '</td>
-          <td>' . $row['Name'] . '</td>
-          <td>' . $row['Surname'] . '</td>
-        </tr>';
-		}
-		$content .= '
-      </tbody>
-    </table>';
-	}
-
+  exit();
 }
+# If user is correctly authenticated:
+$content = '
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Grade</th>
+      <th scope="col">Subject</th>
+      <th scope="col">Professor</th>
+    </tr>
+  </thead>
+  <tbody>';
+$cur_child = $sparent->get_current_child();
+
+//Note: a warning message is already displayed in the index, so for now an ugly die is enough
+if ($cur_child == -1) {
+  die('<div class="alert alert-danger" role="alert">Please, select a student</div>');
+}
+
+$grades = $sparent->get_grades($cur_child);
+if ($grades) {
+  foreach ($grades as $i => $row) {
+    //TODO: CHANGE WHEN get_grades return correct data
+    //NOT t.Name, Mark, Timestamp, u.Surname (u.surname is useless)
+    $content .= '<tr>
+        <th scope="row">' . $row['Timestamp'] . '</th>
+        <td>' . $row['Mark'] . '</td>
+        <td>' . $row['Name'] . '</td>
+        <td>' . $row['Surname'] . '</td>
+      </tr>';
+  }
+  $content .= '
+    </tbody>
+  </table>';
+}
+
 
 $page->setContent($content);
 $site->render();
