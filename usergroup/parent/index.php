a57d72a69459b8c24590bcfac8ab5725bf0447f3
Emanuele Munafò <ema.muna95@gmail.com>
2019-12-10 18:44:55 +0100

    Added html template for timeline

--- usergroup/parent/index.php
+++ usergroup/parent/index.php
@@ -52,7 +52,6 @@ if ($_GET['action'] == "switchChild") {
 	}
 	$content = '
 				
-		
 		<div class="card">
 			<h2 style="background-color:rgba(108,108,108,0.9);color:white" class="card-header info-color white-text text-center py-4">
 				Logged as: parent - '.$sparent->get_name().' '.$sparent->get_surname().'
@@ -94,6 +93,61 @@ if ($_GET['action'] == "switchChild") {
 				</div>
 			</div>
 		</div>';
+
+	// print timeline
+	$content .= '<section id="cd-timeline" class="cd-container">
+	<div class="cd-timeline-block">
+		<div class="cd-timeline-img cd-picture">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg" alt="Picture">
+		</div> <!-- cd-timeline-img -->
+
+		<div class="cd-timeline-content">
+			<h2>Title of Announcement 1</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
+			<a href="#0" class="cd-read-more">Read more</a>
+			<span class="cd-date">Jan 14</span>
+		</div> <!-- cd-timeline-content -->
+	</div> <!-- cd-timeline-block -->
+
+	<div class="cd-timeline-block">
+		<div class="cd-timeline-img cd-movie">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
+		</div> <!-- cd-timeline-img -->
+
+		<div class="cd-timeline-content">
+			<h2>Title of Announcement 2</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde?</p>
+			<a href="#0" class="cd-read-more">Read more</a>
+			<span class="cd-date">Jan 18</span>
+		</div> <!-- cd-timeline-content -->
+	</div> <!-- cd-timeline-block -->
+
+	<div class="cd-timeline-block">
+		<div class="cd-timeline-img cd-picture">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg" alt="Picture">
+		</div> <!-- cd-timeline-img -->
+
+		<div class="cd-timeline-content">
+			<h2>Title of Announcement 3</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
+			<a href="#0" class="cd-read-more">Read more</a>
+			<span class="cd-date">Jan 24</span>
+		</div> <!-- cd-timeline-content -->
+	</div> <!-- cd-timeline-block -->
+
+	<div class="cd-timeline-block">
+		<div class="cd-timeline-img cd-movie">
+			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
+		</div> <!-- cd-timeline-img -->
+
+		<div class="cd-timeline-content">
+			<h2>Final Announcement</h2>
+			<p>This is the content of the last Announcement</p>
+			<span class="cd-date">Feb 26</span>
+		</div> <!-- cd-timeline-content -->
+	</div> <!-- cd-timeline-block -->
+</section> <!-- cd-timeline -->';
+$content .= '  <script  src="'.PLATFORM_PATH.'/js/timeline.js"></script>';
 }
 
 $page->setContent($content);
