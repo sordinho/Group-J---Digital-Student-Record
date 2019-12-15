177c25e0f2ad017b998399a357b17ffc5e08dccd
Vittorio <vittoriodl21@gmail.com>
2019-12-15 18:19:14 +0100

    Update registerNote

--- usergroup/teacher/registerNote.php
+++ usergroup/teacher/registerNote.php
@@ -53,6 +53,7 @@ if (isset($_GET['operation_result'])) {
                 </div>';
 
     } else if (isset($_GET['classID'])) {
+        $_SESSION['classID'] = $_GET['classID'];
         $students_info = $teacher->get_students_by_class_id($_GET['classID']);
         // let's assume there is no need to associate subject to absences
         //if(isset())
@@ -134,6 +135,7 @@ OUT;
 
         // If a form was just sent, elaborate its data and perform operation on backend
     } else if (!empty($_POST)) {
+        $classID = $_SESSION['classID'];
         $students_info = $teacher->get_students_by_class_id($classID);
         if (sizeof($students_info) == 0) {
             $teacher->get_error(24);
