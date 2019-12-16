17e9a3cc9c383dffdee7d788f77d98e9004e7057
Riccardo Mamone <s265425@studenti.polito.it>
2019-12-16 17:28:16 +0100

    merge

--- classes/officer.class.php
+++ classes/officer.class.php
@@ -357,6 +357,11 @@ class officer extends user
     {
         // 5 righe (5 giorni ) x 6 ore
         // in ogni casella teacherID_topicID
+        for ($i = 0; $i < calendar::get_day_per_school_week(); $i++) {
+            for ($j = 0; $j < calendar::get_hour_per_school_day(); $j++) {
+
+            }
+        }
         return true;
     }
 
