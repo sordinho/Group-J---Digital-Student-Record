d0ae94adbd5a0522316ba1ba942a1e7a6cd58f7c
francesco <frenky.riba.95@gmail.com>
2019-12-16 18:49:50 +0100

    tweaks

--- classes/officer.class.php
+++ classes/officer.class.php
@@ -363,26 +363,48 @@ class officer extends user
     /**
      * topicID|teacherID|insert
      * @param $data
+     * @param $classID
      * @return bool
      */
     public function set_timetable_class($data, $classID)
     {
-        if (!(isset($data["hours"]) && isset($data["classID"]))) {
+        if (!(isset($data) && isset($classID))) {
             return false;
         }
-
+        $hourSlot=0;
+        $day=0;
         $conn = $this->connectMySQL();
-        //user.Name, user.Surname, topic.Name, topic.ID, teacher.ID
-        for ($i = 0; $i < calendar::get_day_per_school_week(); $i++) {
-            for ($j = 0; $j < calendar::get_hour_per_school_day(); $j++) {
-                $pieces = explode("_", $data["hours"][$i][$j]);
-                $stmt = $conn->prepare("INSERT INTO Timetables (TeacherID, TopicID, SpecificClassID,HourSlot,DayOfWeek) VALUES (?,?,?,?,?);");
-                $stmt->bind_param('iiiii', intval($pieces[1]), intval($pieces[0]), $data["classID"], $j, $i);
+        for ($day = 0; $day < calendar::get_days_per_school_week(); $day++) {
+            for ($hourSlot = 0; $hourSlot < calendar::get_hours_per_school_day(); $hourSlot++) {
+                $pieces = explode("|", $data[$hourSlot][$day]);
+                if($pieces[2]=="update"){
+                    // update nel DB
+                    $stmt = $conn->prepare("UPDATE Timetables SET TeacherID = ?, TopicID = ?
+                                                    WHERE SpecificClassID = ?
+                                                    AND DayOfWeek = ? 
+                                                    AND HourSlot = ?;");
+                    $stmt->bind_param('iiiii',intval($pieces[1]),intval($pieces[0]), $classID, $day, $hourSlot);
+                    echo "Update";
+                } else if ($pieces[2]=="insert"){
+                    // insert nel DB
+                    $stmt = $conn->prepare("INSERT INTO Timetables (TeacherID, TopicID, SpecificClassID,HourSlot,DayOfWeek) VALUES (?,?,?,?,?);");
+                    echo "Dati:  TeacherID: ".$pieces[1]."  TopicID: ".$pieces[0]."  SpecificClassID: ".$classID."  HourSlot: ".($hourSlot+1)."  DayOfWeek: ".($day+1);
+                    $par1=intval($pieces[1]);
+                    $par2=intval($pieces[0]);
+                    $par4=intval($hourSlot);
+                    $par5=intval($day);
+
+                    $stmt->bind_param('iiiii', $par1, $par2, $classID, $par4, $par5);
+                    echo "Insert";
+                }else{
+                    echo "Errore1";
+                    return false;
+                }
                 if (!$stmt->execute()) {
                     return false;
                 }
             }
-            $j = 0;
+            $hourSlot = 0;
         }
         return true;
     }
@@ -466,7 +488,7 @@ class officer extends user
                          */
                         $timetable[$row['HourSlot']][$row['DayOfWeek']][] = $row;
                         while ($row2 = $res2->fetch_assoc()) {
-                            $row2['action'] = 'insert';
+                            $row2['action'] = 'update';
                             $timetable[$row['HourSlot']][$row['DayOfWeek']][] = $row2;
                         }
                     } else {
--- usergroup/officer/setClassTimetable.php
+++ usergroup/officer/setClassTimetable.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once("../../config.php");
+require_once ("../../classes/calendar.class.php");
 
 $site = new csite();
 initialize_site($site);
@@ -26,7 +27,7 @@ if (!empty($_POST)) {
 
     }
 
-    if ($officer->setTimeTableClass($teacher_hour_day,$_SESSION['classID'])) {
+    if ($officer->set_timetable_class($teacher_hour_day,$_SESSION['classID'])) {
         $content = '
 			<div class="alert alert-success" role="warning">
 			Timetable saved correctly <a href="./setClassTimetable.php" class="alert-link">just click here!</a>
@@ -347,7 +348,7 @@ else {
     $content = '<div class="card">
 
                 <h2 class="card-header info-color white-text text-center py-4" style="background-color:rgba(108,108,108,0.9);color:white">
-                    Change class composition for:
+                    Change timetable of class:
                 </h2>
                 <div class="card-body  px-lg-5 pt-0 mt-md-5 ">';
     $content .=
