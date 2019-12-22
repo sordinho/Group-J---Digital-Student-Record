13b1bc15e975328a6538851add321cd2bec37538
Antonio Santoro <a.santoro1993@gmail.com>
2019-12-22 17:00:59 +0100

    index.php login logic fix for showing spinner during login phase

--- index.php
+++ index.php
@@ -9,22 +9,59 @@ $site->setPage($page);
 
 $content = "";
 
-if (isset($_SESSION['id']) && isset($_POST['username']) && !isset($_SESSION['usergroup'])) {
+if (isset($_POST['usergroup'])) {
 	$usr = new user();
-	if (isset($_POST['usergroup'])) {
-		if ($usr->select_usergroup($_POST['usergroup'])) {
-			$url = $usr->get_base_url();
-			$content .= "<meta http-equiv='refresh' content='1; url=" . PLATFORM_PATH . $url . "' />";
-		} else {
-			$usr->get_error(26);
-		}
+	if ($usr->select_usergroup($_POST['usergroup'])) {
+		$url = PLATFORM_PATH.$usr->get_base_url();
+		header("location: $url");
 	} else {
-		$usergroup = $usr->retrieve_usergroups($usr->get_username());
-		foreach ($usergroup as $ug) {
-			$content .= $ug." ";
-		}
-		$content .= "select an usergroup";
+		$usr->get_error(26);
 	}
+}
+
+if (isset($_SESSION['id']) && isset($_SESSION['username']) && !isset($_SESSION['usergroup']) && $_GET['select_usergroup']==1) {
+	$usr = new user();
+	$usergroup = $usr->retrieve_usergroups($usr->get_username());
+	foreach ($usergroup as $ug) {
+		$icon = "";
+		switch ($ug) {
+			case 'teacher':
+				$icon = "fas fa-briefcase";
+				break;
+			case 'parent':
+				$icon = "fas fa-child";
+				break;
+			case 'officer':
+				$icon = "fas fa-user-cog";
+				break;
+			case 'admin':
+				$icon = "fas fa-tools";
+				break;
+		}
+		$roles .= "
+		<div class='col-3'>
+		<form method='POST' action='index.php'>
+				<button class='card-role' style='border: none; background: none' type='submit' name='usergroup' value='$ug'>
+					  <i class='$icon fa-2x'></i>
+					  <div class='container-role'>
+						<h5><b>$ug</b></h5>
+					  </div>
+				</button>
+		</form>
+		</div>
+		";
+	}
+	$content .= "
+				<div class=\"card text-center\" id='usergroup-selection'>
+				<div class=\"card-header\" style=\"background-color:rgba(108,108,108,0.9);color:white\">
+					<p class='h3'>Select a role for the login</p>
+				</div>
+				<div class=\"card-body row\">
+					$roles
+				</div>
+			</div>
+	";
+
 } elseif (!isset($_SESSION['id']) && isset($_POST['username'])) {
 	/*if (!isset($_POST['usergroup'])) {
 		$url = 'location: '.PLATFORM_PATH.'/error.php?errorID=20';
@@ -58,22 +95,25 @@ if (isset($_SESSION['id']) && isset($_POST['username']) && !isset($_SESSION['use
 			$content .= "<meta http-equiv='refresh' content='1; url=" . PLATFORM_PATH . $url . "' />";
 			break;
 
-		case -1:
+		/*case -1:
 			$usr->get_error(11);
-			break;
+			break;*/
 
 		case 2:
-			$url = "/index.php";
-			$content .= "<meta http-equiv='refresh' content='1; url=" . PLATFORM_PATH . $url . "' />";
+			$url = PLATFORM_PATH."/index.php?select_usergroup=1";
+			$content .= "<meta http-equiv='refresh' content='1; url=" . $url . "' />";
+//			header("location: $url");
 			break;
 
 		default:
-//			$usr->get_error(11);
+			$usr->get_error(11);
 			break;
 	}
 } else {
 	$usr = new user();
-	if($usr->is_logged()){
+	if (isset($_SESSION['id']) && isset($_SESSION['username']) && !isset($_SESSION['usergroup'])) {
+		$content .= "<meta http-equiv='refresh' content='0; url=" . PLATFORM_PATH . "/index.php?select_usergroup=1' />";
+	} elseif($usr->is_logged() && isset($_SESSION['usergroup'])){
 		$content .= "<meta http-equiv='refresh' content='0; url=" . PLATFORM_PATH . $usr->get_base_url() . "' />";
 	}
 }
